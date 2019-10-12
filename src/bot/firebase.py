9af8d280f9255b68e7c8856a2512fca2f8777cfd
yumechi <yumechi0525@gmail.com>
2019-10-12 17:48:54 +0900

    diff member function

--- src/bot/firebase.py
+++ src/bot/firebase.py
@@ -5,32 +5,92 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 dot_env_file = ".env"
+firebase_cred = None
 
 
-class FirebaseSettingData:
+def init_firebase():
+    # 1度しかInitしてはいけないため
+    global firebase_cred
+    if firebase_cred:
+        return
+    load_dotenv(dot_env_file)
+
+    credentials_file_name = os.environ.get("FIREBASE_FILENAME")
+    firebase_cred = credentials.Certificate(credentials_file_name)
+    firebase_admin.initialize_app(firebase_cred)
+
+
+def content_diff(
+    connpass_data: dict, firebase_data: dict, diff_key: str
+) -> dict:
+    diff = {}
+    for kind_key, kind_info in connpass_data.items():
+        kind_diff = {"add": [], "sub": []}
+        latest_data = kind_info.get(diff_key, [])
+        if kind_key not in firebase_data:
+            kind_diff["add"] = latest_data
+        else:
+            old_data = firebase_data[kind_key].get(diff_key, [])
+            kind_diff["add"] = list(set(latest_data) - set(old_data))
+            kind_diff["sub"] = list(set(old_data) - set(latest_data))
+        diff[kind_key] = kind_diff
+    return diff
+
+
+class FirebaseConnector:
     def __init__(self, content_name):
-        load_dotenv(dot_env_file)
-
-        credentials_file_name = os.environ.get("FIREBASE_FILENAME")
-        cred = credentials.Certificate(credentials_file_name)
-        firebase_admin.initialize_app(cred)
-
+        self.content_name = content_name
+        init_firebase()
         try:
             db = firestore.client()
-            self.db_ref = db.collection(content_name).stream()
+            self.connection = db.collection(content_name)
         except Exception as e:
             print("firebase connection error: %s" % e)
             raise e
 
-    def get_event(self):
+
+class FirebaseDataGetter(FirebaseConnector):
+    def get_data(self, **kwargs):
+        if hasattr(self, "content_name"):
+            return getattr(self, "get_" + self.content_name, self.get_none)(
+                **kwargs
+            )
+        return {}
+
+    def get_none(self):
+        return {}
+
+    def get_notifications(self):
         # generatorなので
         try:
-            return [data for data in self.db_ref][0].to_dict()
+            ref = self.connection.stream()
+            return [data for data in ref][0].to_dict()
         except Exception as e:
             print("get event error: %s" % e)
             raise e
 
+    def get_event_members(self, event_key):
+        # generatorなので
+        try:
+            ref = self.connection.stream()
+            for doc in ref:
+                if doc.id == event_key:
+                    return doc.to_dict()
+        except Exception as e:
+            print("get members error: %s" % e)
+            return {}
+
+
+class FirebaseDataSetter(FirebaseConnector):
+    def set_members(self, key, data):
+        try:
+            doc_ref = self.connection.document(key)
+            doc_ref.set(data)
+        except Exception as e:
+            print("set event error: %s" % e)
+            raise e
+
 
 if __name__ == "__main__":
-    entity = FirebaseSettingData("notifications")
-    print(entity.get_event())
+    entity = FirebaseDataGetter("notifications")
+    print(entity.get_data())
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from bot.firebase import FirebaseSettingData
+from bot.firebase import FirebaseDataGetter, FirebaseDataSetter, content_diff
 from scraping.connpass import scraping_run
 from bot.slack.slack_cron import slack_post
 
@@ -6,25 +6,62 @@ from bot.slack.slack_cron import slack_post
 class Application:
     @staticmethod
     def run():
-        firebase_data = FirebaseSettingData("notifications")
-        for post_setting in firebase_data.get_event().values():
+        notifications = FirebaseDataGetter("notifications")
+        for event_key, post_setting in notifications.get_data().items():
             result = scraping_run(post_setting["event_url"])
 
-            message_header = f"{post_setting['event_name']}の参加者 "
-            message_list = [message_header]
-            for key, value in result.items():
-                category = key
-                member_count = str(value["member_count"]) + "人"
-                member = ", ".join(value["member"])
-                message = f"{category} [{member_count}]: {member}"
-                message_list.append(message)
-            # TODO: このへんで差分を取るプログラムを書く
+            message = Application.create_message(
+                result, event_key, post_setting["event_name"]
+            )
             # TODO: 更新結果のプログラムを書く
+            FirebaseDataSetter("event_members").set_members(event_key, result)
             for setting in post_setting["send_to"]:
                 if not bool(setting.get("enable")):
                     continue
-                setting["message"] = "\n".join(message_list)
+                setting["message"] = message
                 slack_post(**setting)
+
+    @staticmethod
+    def create_message(result, event_key, event_name):
+        message_header = f"{event_name}の参加者 "
+        message_list = [message_header]
+
+        for key, value in result.items():
+            category = key
+            member_count = str(value["member_count"]) + "人"
+            member = ", ".join(value["member"])
+            message = f"{category} [{member_count}]: {member}"
+            message_list.append(message)
+        event_members = FirebaseDataGetter("event_members").get_data(
+            event_key=event_key
+        )
+        diff = content_diff(
+            connpass_data=result,
+            firebase_data=event_members,
+            diff_key="member",
+        )
+
+        diff_message = [""]
+        for key, diff in diff.items():
+            add_diff, sub_diff = diff["add"], diff["sub"]
+            if not add_diff and not sub_diff:
+                continue
+            single_diff_header = "+++++++++++++++++++++++++++++++++++++++++"
+            single_diff_message = [single_diff_header, key]
+            if add_diff:
+                add_member_content = (
+                    f"新しい参加者 😀 {len(add_diff)}人: {'、'.join(add_diff)}"
+                )
+                single_diff_message.append(add_member_content)
+            if sub_diff:
+                sub_member_content = (
+                    f"新しいキャンセル者 😰{len(sub_diff)}人: {'、'.join(sub_diff)}"
+                )
+                single_diff_message.append(sub_member_content)
+            single_diff_message.append(single_diff_header)
+            diff_message.append("\n".join(single_diff_message))
+        message_list.append("\n".join(diff_message))
+        return "\n".join(message_list)
 
 
 if __name__ == "__main__":
